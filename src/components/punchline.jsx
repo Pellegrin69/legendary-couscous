a6d63e26799e812e806c0b06f0a0c83e9fb4a799
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2021-01-13 11:27:55 +0100

    Affichage en liste des artistes

--- src/components/punchline.jsx
+++ src/components/punchline.jsx
@@ -14,28 +14,12 @@ class Lyrics extends Component {
         return (
             <>
                 <p>Il y a {lyricsCount} punchlines dans la base !</p>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>Punchline</th>
-                        <th>Artiste</th>
-                        <th>Titre</th>
-                        <th>Album</th>
-                        <th>Annee</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.lyrics.map((lyric) => (
-                        <tr key={lyric._id}>
-                            <td>{lyric.punch}</td>
-                            <td>{lyric.artiste}</td>
-                            <td>{lyric.titre}</td>
-                            <td>{lyric.album}</td>
-                            <td>{lyric.annee}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+
+                {this.state.lyrics.map((lyric) => (
+                    <ul key={lyric._id}>
+                        <li>{lyric.artiste}</li>
+                    </ul>
+                ))}
             </>
         );
     }
