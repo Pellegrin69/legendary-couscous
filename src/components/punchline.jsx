67c5fa282c784d5e6d683967ab531e2921e1a331
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2021-01-13 17:56:37 +0100

    Ajout fillArtistArray()

--- src/components/punchline.jsx
+++ src/components/punchline.jsx
@@ -4,7 +4,16 @@ import {getLyrics} from "../services/lyricsService";
 class Lyrics extends Component {
     state = {
         lyrics: getLyrics(),
+
     };
+
+    fillArtistArray() {
+        let artistArray = []
+        this.state.lyrics.map((artist) =>
+            artistArray.push(artist.artiste.name))
+        console.log(artistArray)
+        return artistArray
+    }
 
     render() {
         const lyricsCount = this.state.lyrics.length;
@@ -14,6 +23,7 @@ class Lyrics extends Component {
         return (
             <>
                 <p>Il y a {lyricsCount} punchlines dans la base !</p>
+                {this.fillArtistArray()}
                 <ul>{this.state.lyrics.map((lyric) =>
                     <li key={lyric.artiste._id}>{lyric.artiste.name}</li>)}
                 </ul>
